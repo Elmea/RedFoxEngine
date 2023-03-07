e4043303d4c4cb3a82e36c8338e90b1b402c47da
Victor CARAULAN <v.caraulan@student.isartdigital.com>
2023-03-07 18:17:42 +0100

    changed to spaces.2

--- include/OpenGLGraphics.hpp
+++ include/OpenGLGraphics.hpp
@@ -5,49 +5,51 @@
 #include "Win32Platform.hpp"
 #endif
 
-#include "ObjParser.hpp"
 #include "Model.hpp"
-#include <GL/gl.h>
+#include "ObjParser.hpp"
 #include "OpenGLFunctions.hpp"
+#include <GL/gl.h>
 
 namespace RedFoxEngine
 {
 
-	struct Light {
-		vec3 position;
-		vec3 direction;
-		float cutOff;
-		float outerCutOff;
+struct Light
+{
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
 
-		float constant;
-		float linear;
-		float quadratic;
+    float constant;
+    float linear;
+    float quadratic;
 
-		vec3 ambient;
-		vec3 diffuse;
-		vec3 specular;
-	};
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
 
-	class Graphics
-	{
-	private:
-		RedFoxMaths::Mat4 m_viewProjection;
-		GLuint m_pipeline;
-		GLuint m_vshader, m_fshader;
-	public:
-		void Draw(Model *model, int modelCount);
-		void DrawModel(Model model);
-		void InitModel(Model *model);
-		void InitTexture(ObjModel *model);
-		void InitShaders();
-		void InitGraphics();
-		void SetViewProjectionMatrix(RedFoxMaths::Mat4 vp);
-	};
-}
+class Graphics
+{
+  private:
+    RedFoxMaths::Mat4 m_viewProjection;
+    GLuint m_pipeline;
+    GLuint m_vshader, m_fshader;
 
-extern PFNWGLCHOOSEPIXELFORMATARBPROC    wglChoosePixelFormatARB;
+  public:
+    void Draw(Model *model, int modelCount);
+    void DrawModel(Model model);
+    void InitModel(Model *model);
+    void InitTexture(ObjModel *model);
+    void InitShaders();
+    void InitGraphics();
+    void SetViewProjectionMatrix(RedFoxMaths::Mat4 vp);
+};
+} // namespace RedFoxEngine
+
+extern PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB;
 extern PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB;
-extern PFNWGLSWAPINTERVALEXTPROC         wglSwapIntervalEXT;
+extern PFNWGLSWAPINTERVALEXTPROC wglSwapIntervalEXT;
 
 #define X(type, name) extern type name;
 GL_FUNCTIONS(X)
