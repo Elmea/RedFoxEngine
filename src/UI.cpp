642c63b812617cc4198d947455f48a254874a965
vegasword <a.perche@student.isartdigital.com>
2023-03-18 11:24:16 +0100

    Fixed SceneGraph ImGui context

--- src/UI.cpp
+++ src/UI.cpp
@@ -7,22 +7,13 @@ void Engine::DrawSceneNodes(bool is_child, GameObject* gameObj)
     int childrenCount = gameObj->GetChildrenCount(m_gameObjects, m_gameObjectCount);
 
     ImGuiTreeNodeFlags flags;
-    if (is_child)
-    {
+    if (is_child && childrenCount == 0)
         flags = ImGuiTreeNodeFlags_Bullet;
-    }
     else
-    {
-        if (childrenCount == 0)
-            flags = ImGuiTreeNodeFlags_Leaf;
-        else
-            flags = ImGuiTreeNodeFlags_OpenOnArrow;
-    }
-    if (gameObj == m_selectedObject)
+        flags = (childrenCount == 0) ? ImGuiTreeNodeFlags_Leaf : ImGuiTreeNodeFlags_OpenOnArrow;
+    if (gameObj == m_selectedObject) 
         flags |= ImGuiTreeNodeFlags_Selected;
-
-    flags |= ImGuiTreeNodeFlags_SpanFullWidth |
-             ImGuiTreeNodeFlags_OpenOnDoubleClick;
+    flags |= ImGuiTreeNodeFlags_SpanFullWidth | ImGuiTreeNodeFlags_OpenOnDoubleClick;
 
     bool nodeOpen = ImGui::TreeNodeEx(gameObj->name, flags, "%s", gameObj->name);
     if (ImGui::IsItemClicked() && !ImGui::IsItemToggledOpen())
@@ -30,35 +21,34 @@ void Engine::DrawSceneNodes(bool is_child, GameObject* gameObj)
         m_selectedObject = gameObj;
     }
 
+    if (ImGui::BeginDragDropSource())
+    {
+        ImGui::SetDragDropPayload("_SCENENODE", &gameObj, sizeof(&gameObj));
+        ImGui::Text("Moving %s", gameObj->name);
+        ImGui::EndDragDropSource();
+    }
+
+    if (ImGui::BeginDragDropTarget())
+    {
+        if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("_SCENENODE"))
+        {
+            if (payload->IsDelivery())
+            {
+                GameObject* movedGameobject = *(GameObject**)payload->Data;
+                movedGameobject->parent = gameObj;
+            }
+            ImGui::EndDragDropTarget();
+        }
+    }
+    
     if (nodeOpen)
     {
-        if (ImGui::BeginDragDropSource())
-        {
-            ImGui::SetDragDropPayload("_SCENENODE", &gameObj, sizeof(&gameObj));
-            ImGui::EndDragDropSource();
-        }
-        
-        if (ImGui::BeginDragDropTarget())
-        {
-            if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("_SCENENODE"))
-            {
-                if (payload->IsDelivery())
-                {
-                    GameObject* movedGameobject = *(GameObject**)payload->Data;
-                    printf("%s is move into %s\n", movedGameobject->name, gameObj->name);
-                    movedGameobject->parent = gameObj;
-                }
-                ImGui::EndDragDropTarget();
-            }
-        }
-
         if (childrenCount)
         {
             GameObject **children = gameObj->GetChildren(m_gameObjects, m_gameObjectCount, &m_tempAllocator);
             for (int i = 0; i < childrenCount; i++)
                 DrawSceneNodes(true, children[i]);
         }
-
         ImGui::TreePop();
     }
 }
@@ -69,23 +59,38 @@ void Engine::DrawIMGUI()
     ImGui_ImplOpenGL3_NewFrame();
     ImGui::NewFrame();
 
-    ImGuiDockNodeFlags dockingFlags = ImGuiDockNodeFlags_PassthruCentralNode |
-                                      ImGuiDockNodeFlags_NoWindowMenuButton |
-                                      ImGuiDockNodeFlags_NoCloseButton |
-                                      ImGuiDockNodeFlags_NoDockingInCentralNode;
+    ImGuiDockNodeFlags dockingFlags =
+        ImGuiDockNodeFlags_PassthruCentralNode |
+        ImGuiDockNodeFlags_NoWindowMenuButton |
+        ImGuiDockNodeFlags_NoCloseButton |
+        ImGuiDockNodeFlags_NoDockingInCentralNode;
 
     // TODO(a.perche) : Bind widgets to dockspace
     ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(), dockingFlags);
 
     if (ImGui::Begin("Scene Graph", (bool*)0, ImGuiWindowFlags_NoCollapse))
     {
-        ImGuiTreeNodeFlags rootNodeFlags = ImGuiTreeNodeFlags_Framed |
+        ImGuiTreeNodeFlags rootNodeFlags = 
+            ImGuiTreeNodeFlags_Framed |
             ImGuiTreeNodeFlags_Leaf |
             ImGuiTreeNodeFlags_DefaultOpen |
             ImGuiTreeNodeFlags_SpanFullWidth;
 
         if (ImGui::TreeNodeEx("_TREENODE", rootNodeFlags, "Sample Scene"))
         {
+            if (ImGui::BeginDragDropTarget())
+            {
+                if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("_SCENENODE"))
+                {
+                    if (payload->IsDelivery())
+                    {
+                        GameObject* movedGameobject = *(GameObject**)payload->Data;
+                        movedGameobject->parent = nullptr;
+                    }
+                    ImGui::EndDragDropTarget();
+                }
+            }
+
             ImGui::Unindent();
             for (int i = 0; i < (int)m_gameObjectCount; i++)
                 if (m_gameObjects[i].parent == nullptr)
