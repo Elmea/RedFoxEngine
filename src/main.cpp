6e15af54c8a5eab4ef35d98bb3d01f6357fc22f3
Victor CARAULAN <v.caraulan@student.isartdigital.com>
2023-04-04 13:55:21 +0200

    Physx 5.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,56 @@
 #include "Engine.hpp"
-#include "Jolt.h"
+#include "PxConfig.h"
+#include "PxPhysicsAPI.h"
+
+using namespace physx;
+
+static PxDefaultAllocator		gAllocator;
+static PxDefaultErrorCallback	gErrorCallback;
+static PxFoundation* gFoundation = NULL;
+static PxPhysics* gPhysics = NULL;
+static PxDefaultCpuDispatcher* gDispatcher = NULL;
+static PxScene* gScene = NULL;
+static PxMaterial* gMaterial = NULL;
+static PxPvd* gPvd = NULL;
 
 int main()
 {
-    
-    
+	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
 
-    //RedFoxEngine::Engine engine(1920, 1040);
+	gPvd = PxCreatePvd(*gFoundation);
+	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+	gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
 
-    /*
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale());
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	gDispatcher = PxDefaultCpuDispatcherCreate(2);
+	sceneDesc.cpuDispatcher = gDispatcher;
+	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+	gScene = gPhysics->createScene(sceneDesc);
+
+	PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+	if (pvdClient)
+	{
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	}
+	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 0), *gMaterial);
+	gScene->addActor(*groundPlane);
+
+    RedFoxEngine::Engine engine(1920, 1040);
+
+    
     while (engine.isRunning())
     {
         engine.ProcessInputs();
         engine.Update();
         engine.Draw();
     }
-    */
+    
     return (0);
 }
