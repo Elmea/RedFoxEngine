fc09d0f1ed627d9eae230cd5a02bd9e2064cde2d
n.masseteau <n.masseteau@student.isartdigital.com>
2023-04-11 11:59:15 +0200

    Deleted duplicated lines

--- src/Light.cpp
+++ src/Light.cpp
@@ -17,8 +17,6 @@ Light::Light(LightType lightType)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
         lightInfo.shadowParameters.SHADOW_WIDTH, lightInfo.shadowParameters.SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
     glBindFramebuffer(GL_FRAMEBUFFER, lightInfo.shadowParameters.depthMapFBO);
