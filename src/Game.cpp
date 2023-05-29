6a78fb33817c4144b5cd25bd4ff6c0aa6561514b
Maël JAMMET <maeljammet@live.fr>
2023-05-29 10:53:45 +0200

    math function in game code

--- src/Game.cpp
+++ src/Game.cpp
@@ -41,41 +41,71 @@ UIBEHAVIOUR(UI)
 }
 
 
+
+Mat4 GetTranslation(const Float3& pTranslation)
+{
+    Mat4 result;
+
+    result.mat[0][0] = 1;
+    result.mat[1][1] = 1;
+    result.mat[2][2] = 1;
+    result.mat[3][3] = 1;
+
+    result.mat[0][3] = pTranslation.x;
+    result.mat[1][3] = pTranslation.y;
+    result.mat[2][3] = pTranslation.z;
+
+    return result;
+}
+
+Mat4 GetRotationMatrix(float a, float b, float c, float d) 
+{
+    float matrix[4][4] =
+    { {2 * (a * a + b * b) - 1,    2 * (b * c - d * a),      2 * (b * d + c * a),  0},
+        {2 * (b * c + d * a),         2 * (a * a + c * c) - 1,  2 * (c * d - b * a),  0},
+        {2 * (b * d - c * a),         2 * (c * d + b * a),      2 * (a * a + d * d) - 1,  0},
+        {0,                     0,                  0,              1.f} };
+
+    return matrix;
+}
+
+Mat4 GetScale(const Float3& pScale)
+{
+    Mat4 result;
+    result.mat[0][0] = pScale.x;
+    result.mat[1][1] = pScale.y;
+    result.mat[2][2] = pScale.z;
+    result.mat[3][3] = 1;
+
+    return result;
+}
+
+Mat4 CreateTransformMatrix(const Float3& position, const Quaternion& rotation, const Float3& pScale)
+{
+    float a_temp = rotation.a;
+    float b_temp = rotation.b;
+    float c_temp = rotation.c;
+    float d_temp = rotation.d;
+    return GetTranslation(position) * GetRotationMatrix(a_temp,b_temp,c_temp,d_temp) * GetScale(pScale);
+}
+
 void Gun(RedFoxEngine::GameObject* self, RedFoxEngine::Scene* scene, RedFoxEngine::Input* input, RedFoxEngine::Physx* physx)
 {
-    /*
-    if (IsMouseClicked(ImGuiMouseButton_Left) && !m_imgui.manipulatingGizmo && !m_imgui.lockEditor && m_scene.isPaused)
-    {
-        RedFoxMaths::Mat4 view = m_editorCamera.GetViewMatrix().GetInverseMatrix();
-        physx::PxVec3 origin = { view.mat[0][3], view.mat[1][3], view.mat[2][3] };
-        physx::PxVec3 unitDir = { ray_world.x, ray_world.y, ray_world.z };
-        physx::PxRaycastBuffer hitCalls;
-        if (m_physx.m_scene->raycast(origin, unitDir, m_editorCamera.m_parameters._far, hitCalls, physx::PxHitFlag::eANY_HIT))
-        {
-            physx::PxRaycastHit hit = hitCalls.getAnyHit(0);
-            if (hit.actor)
-            {
-                int hitIndex = hit.actor->getInternalActorIndex();
-                if (hitIndex < (int)m_scene.gameObjectCount && hitIndex > 0)
-                {
-                    m_imgui.selectedObject = hitIndex;
-                    m_imgui.mousePickNodeIndex = hitIndex;
-                }
-            }
-        }
-    }
-
-    */
     if (input->mouseLClick)
     {
-        printf("Clic\n");
-        /*
         RedFoxMaths::Float4 ray_clip = { 0, 0, -1, 1 };
         RedFoxMaths::Float4 ray_eye = scene->m_gameCamera.m_projection.GetInverseMatrix() * ray_clip;
-        ray_eye = { ray_eye.x, ray_eye.y, -1, 0 };
-        RedFoxMaths::Float4 ray_world = scene->m_gameCamera.GetViewMatrix().GetInverseMatrix() * ray_eye;
+        ray_eye = { ray_eye.x, ray_eye.y, -1, 0 };        
+
+        //    return Mat4::CreateTransformMatrix(position, orientation, scale).GetInverseMatrix();
+        //    return GetTranslation(position) * rotation.GetRotationMatrix() * GetScale(pScale);
+
+        RedFoxMaths::Mat4 transformMatrix = CreateTransformMatrix(scene->m_gameCamera.position, scene->m_gameCamera.orientation, scene->m_gameCamera.scale);
+        RedFoxMaths::Float4 ray_world = transformMatrix.GetInverseMatrix() * ray_eye;
+      
         ray_world.Normalize();
-        RedFoxMaths::Mat4 view = scene->m_gameCamera.GetViewMatrix().GetInverseMatrix();
+        transformMatrix = CreateTransformMatrix(scene->m_gameCamera.position, scene->m_gameCamera.orientation, scene->m_gameCamera.scale);
+        RedFoxMaths::Mat4 view = transformMatrix.GetInverseMatrix();
         physx::PxVec3 unitDir = { ray_world.x, ray_world.y, ray_world.z };
         physx::PxVec3 origin = { view.mat[0][3], view.mat[1][3], view.mat[2][3] };
         physx::PxRaycastBuffer hitCalls;
@@ -84,8 +114,8 @@ void Gun(RedFoxEngine::GameObject* self, RedFoxEngine::Scene* scene, RedFoxEngin
             physx::PxRaycastHit hit = hitCalls.getAnyHit(0);
             if (hit.actor) printf("OEUF\n");
         } 
-        */
     }
+    printf("\n");
 }
 
 
@@ -100,7 +130,7 @@ BEHAVIOUR(Player)
     scene->m_gameCamera.orientation = Quaternion::FromEuler(-cameraRotation.x, -cameraRotation.y, cameraRotation.z);
     
     Float3 inputDirection(0, 0, 0);
-    float speed = 300.f;
+    float speed = 150.f;
     if (inputs->W || inputs->Up)    inputDirection.z += -1;
     if (inputs->S || inputs->Down)  inputDirection.z += 1;
     if (inputs->A || inputs->Left)  inputDirection.x += -1;
